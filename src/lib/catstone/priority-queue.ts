c65ffeea91304e769775cf2cbe9aa8ccb9d41f90
Roman <dev@rman.dev>
2026-07-20 02:58:44 +0100

    wip

--- src/lib/catstone/priority-queue.ts
+++ src/lib/catstone/priority-queue.ts
@@ -1,73 +1,75 @@
 // Priority Queue - https://stackoverflow.com/a/42919752
 const pqTop = 0;
-const pqParent = (i) => ((i + 1) >>> 1) - 1;
-const pqLeft = (i) => (i << 1) + 1;
-const pqRight = (i) => (i + 1) << 1;
+const pqParent = (i: number) => ((i + 1) >>> 1) - 1;
+const pqLeft = (i: number) => (i << 1) + 1;
+const pqRight = (i: number) => (i + 1) << 1;
 
-class PriorityQueue {
-  constructor(comparator = (a, b) => a > b) {
+type Comparator<T> = (a: T, b: T) => boolean;
+
+class PriorityQueue<T> {
+  _heap: T[];
+  _comparator: Comparator<T>;
+
+  constructor(comparator: Comparator<T> = (a, b) => (a as any) > (b as any)) {
     this._heap = [];
     this._comparator = comparator;
   }
-  size() {
+
+  push(value: T) {
+    this._heap.push(value);
+    let node = this._heap.length - 1;
+    while (node > pqTop) {
+      const parent = pqParent(node);
+      if (this._comparator(this._heap[node], this._heap[parent])) {
+        const tmp = this._heap[node];
+        this._heap[node] = this._heap[parent];
+        this._heap[parent] = tmp;
+        node = parent;
+      } else {
+        break;
+      }
+    }
     return this._heap.length;
   }
-  isEmpty() {
-    return this.size() == 0;
-  }
-  peek() {
-    return this._heap[pqTop];
-  }
-  push(...values) {
-    values.forEach((value) => {
-      this._heap.push(value);
-      this._siftUp();
-    });
-    return this.size();
-  }
-  pop() {
-    const poppedValue = this.peek();
-    const bottom = this.size() - 1;
+
+  pop(): T {
+    const poppedValue = this._heap[pqTop]!;
+    const bottom = this._heap.length - 1;
     if (bottom > pqTop) {
-      this._swap(pqTop, bottom);
+      this._heap[pqTop] = this._heap[bottom]!;
+      this._heap[bottom] = poppedValue;
     }
     this._heap.pop();
-    this._siftDown();
+
+    let node = pqTop;
+    const len = this._heap.length;
+    while (true) {
+      const left = pqLeft(node);
+      const right = pqRight(node);
+      let best = node;
+
+      if (left < len && this._comparator(this._heap[left], this._heap[best])) {
+        best = left;
+      }
+      if (
+        right < len &&
+        this._comparator(this._heap[right], this._heap[best])
+      ) {
+        best = right;
+      }
+      if (best === node) break;
+
+      const tmp = this._heap[node]!;
+      this._heap[node] = this._heap[best]!;
+      this._heap[best] = tmp;
+      node = best;
+    }
+
     return poppedValue;
   }
-  replace(value) {
-    const replacedValue = this.peek();
-    this._heap[pqTop] = value;
-    this._siftDown();
-    return replacedValue;
-  }
-  _greater(i, j) {
-    return this._comparator(this._heap[i], this._heap[j]);
-  }
-  _swap(i, j) {
-    [this._heap[i], this._heap[j]] = [this._heap[j], this._heap[i]];
-  }
-  _siftUp() {
-    let node = this.size() - 1;
-    while (node > pqTop && this._greater(node, pqParent(node))) {
-      this._swap(node, pqParent(node));
-      node = pqParent(node);
-    }
-  }
-  _siftDown() {
-    let node = pqTop;
-    while (
-      (pqLeft(node) < this.size() && this._greater(pqLeft(node), node)) ||
-      (pqRight(node) < this.size() && this._greater(pqRight(node), node))
-    ) {
-      let maxChild =
-        pqRight(node) < this.size() &&
-        this._greater(pqRight(node), pqLeft(node))
-          ? pqRight(node)
-          : pqLeft(node);
-      this._swap(node, maxChild);
-      node = maxChild;
-    }
+
+  isEmpty() {
+    return this._heap.length === 0;
   }
 }
 
